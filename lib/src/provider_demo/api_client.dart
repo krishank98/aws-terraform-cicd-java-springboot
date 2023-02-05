b3999a22f82b3f5493234f1aaf2cafc0b63adf81
krishank98 <37609865+krishank98@users.noreply.github.com>
2023-02-05 06:05:16 +0000

    adding

--- lib/src/provider_demo/api_client.dart
+++
@@ -1,9 +0,0 @@
-import 'package:retrofit/retrofit.dart';
-import 'package:dio/dio.dart';
-
-part 'api_client.g.dart';
-
-@RestApi(baseUrl: "https://reqres.in/api/")
-abstract class RestClient {
-  factory RestClient(Dio dio) = _RestClient;
-}
